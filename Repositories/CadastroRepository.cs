20d88eb6f482ca4dc1ee11c220429ad6548b6977
Victor Antunes J <victorsergioantunes23@gmail.com>
2023-11-02 20:22:44 -0300

    Criando a pasta Repositories e fazendo a implementação do cadastro

---
+++ Repositories/CadastroRepository.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_SistemaCursosDistancia.Interfaces;
+using Api_SistemaCursosDistancia.Models;
+
+namespace Api_SistemaCursosDistancia.Repositories
+{
+    public class CadastroRepository : ICadastroRepository
+    {
+        public bool Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Cadastro> GetALL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cadastro GetBuId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cadastro Insert(int Id, Cadastro cadastro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cadastro Update(int ID, Cadastro cadastro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
