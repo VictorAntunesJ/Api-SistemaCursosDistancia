35283ec0bb3affb103022d9763585c48eeafe2ed
Victor Antunes J <victorsergioantunes23@gmail.com>
2023-11-05 15:46:00 -0300

    Criação do codigo para retornar o arquivo

--- Utils/Upload.cs
+++ Utils/Upload.cs
@@ -9,10 +9,18 @@ namespace Api-SistemaCursosDistancia.Utils
     {
         // Upload de Imagens.
 
-        // Validar extenções de arquivo.
-
         // Remover arquivo.
 
+        // Validar extensões de arquivo.
+   
         // Retornar extençõe.
+        public static string RetornarExtensao(string nomeArquivo)
+        {
+            // [0]  [1]  [2]
+            // arq.uivo.jpeg = 3
+            // lengh(3) - 1 = 2
+            string[] dados = nomeArquivo.Split('.');
+            return dados[dados.Lengh = 1];
+        }
     }
 }
