300d4f34197421010bb7bc925e40c5292e4b6c33
Victor Antunes J <victorsergioantunes23@gmail.com>
2023-11-13 22:15:49 -0300

    Criando a interface Login

---
+++ Interfaces/ILoginRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_SistemaCursosDistancia.Models;
+
+namespace Api_SistemaCursosDistancia.Interfaces
+{
+    public interface ILoginRepository
+    {
+        Cadastro Logar(string email, string senha);
+    }
+}
